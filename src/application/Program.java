e4e1240f6c08cd23d4b478c996c62ce8a6e1d4ae
Leisson Rodrigues <leisson@gmail.com>
2023-04-16 18:18:58 -0400

    process invoice updated

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,48 @@
 package application;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.CarRental;
+import model.entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalService;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+		System.out.println("Enter with Rental data");
+		System.out.print("Car model: ");
+		String carModel = sc.nextLine();
+
+		System.out.print("Check-in (dd/MM/yyyy hh:mm): ");
+		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+
+		System.out.print("Check-out (dd/MM/yyyy hh:mm): ");
+		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+		
+		CarRental carRental = new CarRental(start, finish, new Vehicle(carModel));
+
+		System.out.print("Enter with price per Hour: ");
+		Double pricePerHour = sc.nextDouble();
+		System.out.print("Enter with price per Day: ");
+		Double pricePerDay = sc.nextDouble();
+		
+		RentalService rs = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+		
+		rs.processInvoice(carRental);
+		
+		System.out.println(carRental.getInvoice().toString());
+		
+		sc.close();
 	}
 
 }
---
+++ src/model/entities/Invoice.java
@@ -0,0 +1,38 @@
+package model.entities;
+
+public class Invoice {
+
+	private Double basicPayment;
+	private Double tax;
+
+	public Invoice() {
+	}
+
+	public Invoice(Double basicPayment, Double tax) {
+		this.basicPayment = basicPayment;
+		this.tax = tax;
+	}
+
+	public Double getBasicPayment() {
+		return basicPayment;
+	}
+
+	public Double getTax() {
+		return tax;
+	}
+
+	public Double getTotalPayment() {
+		return getBasicPayment() + getTax();
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("INVOICE:\n");
+		sb.append("Basic payment: " + String.format("%.2f",getBasicPayment()) + "\n");
+		sb.append("Tax: " + getTax() + "\n");
+		sb.append("Total Payment: " + getTotalPayment() + "\n");
+		return sb.toString();
+	}
+
+}
